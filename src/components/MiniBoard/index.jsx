bf979052a1acb24c8bb23938605ae4ec6983b937
zhengshugan <zhengshugan@foxmail.com>
2022-09-19 19:41:30 +0800

    Add MiniBoard component

---
+++ src/components/MiniBoard/index.jsx
@@ -0,0 +1,22 @@
+const MiniBoard = ({ finalBoard, name, player }) => {
+  return (
+    <div className='mini-board-container'>
+      <div
+        className={`mini-board ${
+          player === 'p1' ? 'mini-board-p1' : 'mini-board-p2'
+        }`}
+        onContextMenu={(e) => e.preventDefault()}
+      >
+        {finalBoard.current.map((state, index) => (
+          <div
+            key={index}
+            className={`block mini ${state}`}
+            data-index={index}
+          ></div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default MiniBoard
